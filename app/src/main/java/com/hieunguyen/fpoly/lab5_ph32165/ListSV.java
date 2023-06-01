6240d906ff0a55f784fb86e9ec010dae122f3b23
HieuNguyenFPOLY <manhhieu.nguyen1805@gmail.com>
2023-06-01 14:10:56 +0700

    Initial commit

---
+++ app/src/main/java/com/hieunguyen/fpoly/lab5_ph32165/ListSV.java
@@ -0,0 +1,13 @@
+package com.hieunguyen.fpoly.lab5_ph32165;
+
+public class ListSV {
+    public String coSo;
+    public String hoTen;
+    public String diaChi;
+
+    public ListSV(String coSo, String hoTen, String diaChi) {
+        this.coSo = coSo;
+        this.hoTen = hoTen;
+        this.diaChi = diaChi;
+    }
+}
---
+++ app/src/main/java/com/hieunguyen/fpoly/lab5_ph32165/School.java
@@ -0,0 +1,10 @@
+package com.hieunguyen.fpoly.lab5_ph32165;
+
+public class School {
+    public int hinh;
+    public String ten;
+    public School(int hinh, String ten) {
+        this.hinh = hinh;
+        this.ten = ten;
+    }
+}
